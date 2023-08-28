c29769241bb04aa6ea81a07143c9d1b65cbc52d6
setiawannuha <setiawannuha123@gmail.com>
2023-08-28 13:24:49 +0700

    change cloudinary upload stream

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const cors = require("cors")
 const cloudinary = require("cloudinary").v2
 const qr = require("qr-image")
 const path = require('path');
-const fs = require('fs');
+const fs = require("fs")
+const streamifier = require('streamifier')
 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_NAME, 
@@ -26,13 +27,14 @@ app.get("/generate", async (req, res) => {
   try {
     const {code} = req.query
     const qrImage = qr.image(code, { type: 'png' });
-    const publicDirectory = path.resolve(process.cwd(), 'public');
-    qrImage.pipe(require('fs').createWriteStream(`${publicDirectory}/${code}.png`));
-    const result = await cloudinary.uploader.upload(`${publicDirectory}/${code}.png`, {public_id: `qrcode/${code}`});
-    return res.json({
-      msg: "success",
-      result
+    const publicDirectory = path.join(process.cwd(), 'public');
+    await qrImage.pipe(require('fs').createWriteStream(`${publicDirectory}/${code}.png`));
+    const file = await fs.readFileSync(`${publicDirectory}/${code}.png`);
+    const stream = cloudinary.uploader.upload_stream({folder: `qrcode`}, (error, result) => {
+      if (error) return console.error(error);
+      res.status(200).json(result);
     })
+    streamifier.createReadStream(file).pipe(stream);
   } catch (error) {
     console.log(error);
     return res.send("Error")
